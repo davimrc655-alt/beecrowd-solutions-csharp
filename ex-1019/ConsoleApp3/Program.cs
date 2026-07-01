d6a91bd0bbd11f88fcbf49981e88143430f2a43b
davimrc655-alt <davimrc655@gmail.com>
2026-06-30 23:22:59 -0300

    exercicio 1019 completo , aguardando refinamento

---
+++ ex-1019/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+    int timeDuracaoSegundos = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture); 
+    int horas = timeDuracaoSegundos/3600; 
+    int minutos = (timeDuracaoSegundos%3600)/60; 
+    int segundos = ((timeDuracaoSegundos%3600)%60); 
+    Console.WriteLine($"{horas}:{minutos}:{segundos}"); 
+    }
+}
