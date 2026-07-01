b8655830d4ad2b85e5ed34224008029a8afa014f
davimrc655-alt <davimrc655@gmail.com>
2026-06-30 23:38:46 -0300

    ex 1020 completo , esperando refinamento

---
+++ ex-1020/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int timeDiasDeVida = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+        int Anos = timeDiasDeVida / 365;
+        int Meses = (timeDiasDeVida % 365) / 30;
+        int Dias = ((timeDiasDeVida % 365) % 30);
+        Console.WriteLine($"{Anos} ano(s)");
+        Console.WriteLine($"{Meses} mes(es)");
+        Console.WriteLine($"{Dias} dia(s)");
+    }
+}
