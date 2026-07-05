847d93c9f2fd17ac008f638190864801767915d2
davimrc655-alt <davimrc655@gmail.com>
2026-07-04 22:09:30 -0300

    exercicio 1038 , realizado aguardando atualizacao

---
+++ ex-1038/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System ; 
+
+class URI
+{
+    static void Main()
+    {
+        string [] codigoEQuantidade = Console.ReadLine().Split(' '); 
+        int x , quantidade ; 
+        double precoUnitario , total; 
+        precoUnitario = 0 ; 
+        x = Convert.ToInt32(codigoEQuantidade[0]); 
+        quantidade = Convert.ToInt32(codigoEQuantidade[1]); 
+
+        if (x == 1)
+        {
+            precoUnitario = 4.00 ; 
+        }
+        else if (x == 2)
+        {
+            precoUnitario = 4.50 ; 
+        }
+        else if (x == 3)
+        {
+            precoUnitario = 5.0 ; 
+        }
+        else if (x == 4)
+        {
+            precoUnitario = 2.0 ; 
+        }
+        else if (x == 5)
+        {
+            precoUnitario = 1.50 ; 
+        }
+        total =  precoUnitario * quantidade ; 
+
+        Console.WriteLine($"Total: R$ {total.ToString("F2")}"); 
+    }
+}
