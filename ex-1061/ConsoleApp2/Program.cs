68a12e31598714a09f151f764feff91c5ab665d1
davimrc655-alt <davimrc655@gmail.com>
2026-07-04 22:33:17 -0300

    Simplifica nomes de variáveis e adiciona comentários explicativos

--- ex-1061/ConsoleApp2/Program.cs
+++ ex-1061/ConsoleApp2/Program.cs
@@ -7,34 +7,35 @@ class Uri
     {
     // rece os dados 
     string[] diaDeInicio = Console.ReadLine().Split(' '); 
-    string[] horasMinutosSegundosInicio = Console.ReadLine().Split(':');
+    string[] tempoInicio = Console.ReadLine().Split(':');
     string[] diaDoFIm = Console.ReadLine().Split(' ');
-    string[] horasMinutosSegundosFim = Console.ReadLine().Split(':');
+    string[] tempoFIm = Console.ReadLine().Split(':');
 
     int diaInicio = Convert.ToInt32(diaDeInicio[1]); 
     int diaFim = Convert.ToInt32(diaDoFIm[1]);
 
-        int horas = Convert.ToInt32(horasMinutosSegundosInicio[0].Trim());
-            int minutos = Convert.ToInt32(horasMinutosSegundosInicio[1].Trim());
-                int segundos = Convert.ToInt32(horasMinutosSegundosInicio[2].Trim());
+    int horas = Convert.ToInt32(tempoInicio[0].Trim());
+    int minutos = Convert.ToInt32(tempoInicio[1].Trim());
+    int segundos = Convert.ToInt32(tempoInicio[2].Trim());
 
-    int horasFim = Convert.ToInt32(horasMinutosSegundosFim[0].Trim());
-        int minutosFim = Convert.ToInt32(horasMinutosSegundosFim[1].Trim());
-            int segundosFim = Convert.ToInt32(horasMinutosSegundosFim[2].Trim());
+    int horasFim = Convert.ToInt32(tempoFIm[0].Trim());
+    int minutosFim = Convert.ToInt32(tempoFIm[1].Trim());
+    int segundosFim = Convert.ToInt32(tempoFIm[2].Trim());
 
     // converte dias , horas , minutos para segundo . 
     int SegundosInicio = (diaInicio * 86400) + (horas * 3600)+ (minutos * 60) + segundos ; 
     int SegundosFimCalc = (diaFim * 86400) + (horasFim * 3600)  + (minutosFim * 60) + segundosFim;
 
-    int TotalsegundosConvertido = SegundosFimCalc - SegundosInicio; 
+    int TotalsegundosConvertido = SegundosFimCalc - SegundosInicio; //-- calculamos o total de segundos entre esse periodo 
 
+        //--- aqui eu faco um calculo simples de divisao e resto , para decompor a todos os momentos disponineis ( dia , hora , minuto ), o que sobrar eu atribuo a segundosRest.
     int diasResultado = TotalsegundosConvertido / 86400 ; 
     TotalsegundosConvertido %= 86400 ; 
-        int horasResultado = TotalsegundosConvertido / 3600 ; 
-        TotalsegundosConvertido %= 3600 ; 
-            int minutosResultado = TotalsegundosConvertido / 60 ; 
-            TotalsegundosConvertido %= 60 ;
-                int segundosRest = TotalsegundosConvertido ; 
+    int horasResultado = TotalsegundosConvertido / 3600 ; 
+    TotalsegundosConvertido %= 3600 ; 
+    int minutosResultado = TotalsegundosConvertido / 60 ; 
+    TotalsegundosConvertido %= 60 ;
+    int segundosRest = TotalsegundosConvertido ; 
 
     Console.WriteLine($"{diasResultado} dia(s)"); 
     Console.WriteLine($"{horasResultado} hora(s)"); 
