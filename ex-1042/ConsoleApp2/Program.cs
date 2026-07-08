7cad6353270288ac5b1c4e5e9f4174afba2299fd
davimrc655-alt <davimrc655@gmail.com>
2026-07-08 11:45:56 -0300

    ex 1042 realizado com sucesso , nivel facil

---
+++ ex-1042/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string[] valores = Console.ReadLine().Split(' ');
+        int[] valoresCrescentes = new int[3];
+        valoresCrescentes[0] = Convert.ToInt32(valores[0]);
+        valoresCrescentes[1] = Convert.ToInt32(valores[1]);
+        valoresCrescentes[2] = Convert.ToInt32(valores[2]);
+        Array.Sort(valoresCrescentes);
+        int a, b, c;
+        a = valoresCrescentes[0];
+        b = valoresCrescentes[1];
+        c = valoresCrescentes[2];
+        //-------------------------------
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine($"{c}\n");
+        Console.WriteLine(valores[0]);
+        Console.WriteLine(valores[1]);
+        Console.WriteLine(valores[2]);
+
+    }
+}
