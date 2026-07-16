eb754fe8a2a3222fd0b44bcb69c33376f8e71e2b
davimrc655-alt <davimrc655@gmail.com>
2026-07-16 09:00:13 -0300

    ex de estruturas repetitivas while , verificacao de plano cartesiano

---
+++ ex-1115/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class Uri
+{
+    static void Main(string[] args)
+    {
+        int x, y;
+        string[] valores = Console.ReadLine().Split(' ');
+        x = Convert.ToInt32(valores[0]);
+        y = Convert.ToInt32(valores[1]);
+        while (true)
+        {
+            if (x == 0 || y == 0)
+            {
+                break;
+            }
+            else
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                    valores = Console.ReadLine().Split(' ');
+                    x = Convert.ToInt32(valores[0]);
+                    y = Convert.ToInt32(valores[1]);
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                    valores = Console.ReadLine().Split(' ');
+                    x = Convert.ToInt32(valores[0]);
+                    y = Convert.ToInt32(valores[1]);
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                    valores = Console.ReadLine().Split(' ');
+                    x = Convert.ToInt32(valores[0]);
+                    y = Convert.ToInt32(valores[1]);
+                }
+                else
+                {
+                    Console.WriteLine("quarto");
+                    valores = Console.ReadLine().Split(' ');
+                    x = Convert.ToInt32(valores[0]);
+                    y = Convert.ToInt32(valores[1]);
+                }
+            }
+        }
+    }
+}
